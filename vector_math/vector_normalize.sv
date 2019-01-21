ac1c0d6554d85aeed2df23fdd614a2a7c7411bf6
Thomas Carotti <thomas@carotti.co.uk>
2019-01-21 17:48:42 -0500

    Add Vector normalization testbench

--- vector_math/vector_normalize.sv
+++ vector_math/vector_normalize.sv
@@ -1,5 +1,8 @@
 `timescale 1ns/1ps
 
+// Since the sign bit will be set, we can remove one pipeline stage
+`define PIPELINE_STAGES (`FIXED_W - 1)
+
 // Can't overflow, but answers are very inaccurate for large vectors
 module vector_normalize
 (
@@ -13,27 +16,31 @@ module vector_normalize
     int i;
     genvar g;
 
-    reg vector_t [`FIXED_W:0] temp_ops;
+    reg vector_t [`PIPELINE_STAGES:0] temp_ops;
 
-    reg fixed_point_t [`FIXED_W:0] scalars;
+    reg fixed_point_t [`PIPELINE_STAGES:0] scalars;
 
-    wire vector_t [`FIXED_W:0] scalar_results;
-    wire [`FIXED_W:0] scalar_overflow;
+    wire vector_t [`PIPELINE_STAGES:0] scalar_results;
+    wire [`PIPELINE_STAGES:0] scalar_overflow;
 
-    wire fixed_point_t [`FIXED_W-1:0] dot_results;
-    wire [`FIXED_W-1:0] dot_overflow;
+    // Don't care about the dot product in the final pipeline stage
+    // It should be 1 (or at least very close to 1)
+    wire fixed_point_t [`PIPELINE_STAGES-1:0] dot_results;
+    wire [`PIPELINE_STAGES-1:0] dot_overflow;
+    wire signed [`FIXED_W-1:0] [`PIPELINE_STAGES-1:0] packed_dot_results;
+    wire [`PIPELINE_STAGES-1:0] dot_leq_one;
 
-    wire fixed_point_t test_bits [`FIXED_W:0];
-
-    wire signed [`FIXED_W-1:0] [`FIXED_W-1:0] packed_dot_results;
     wire signed [`FIXED_W-1:0] one;
-    wire [`FIXED_W-1:0] dot_leq_one;
+
+    wire fixed_point_t test_bits [`PIPELINE_STAGES:0];
+
+    // Test bit of the final pipeline stage is always 0
+    assign test_bits[`PIPELINE_STAGES] = 0;
 
     assign one = (1 << `FIXED_FRACTION_W);
-    assign result = scalar_results[`FIXED_W];
-    assign test_bits[`FIXED_W] = 0;
+    assign result = scalar_results[`PIPELINE_STAGES];
 
-    for (g = 0; g <= `FIXED_W; g = g + 1) begin : gen_scalar_mul
+    for (g = 0; g <= `PIPELINE_STAGES; g = g + 1) begin : gen_scalar_mul
         vector_scalar_mul smul (
             .vector_op(temp_ops[g]),
             .scalar_op(scalars[g] | test_bits[g]),
@@ -42,7 +49,7 @@ module vector_normalize
         );
     end
 
-    for (g = 0; g < `FIXED_W; g = g + 1) begin : gen_vector_dot
+    for (g = 0; g < `PIPELINE_STAGES; g = g + 1) begin : gen_vector_dot
         vector_dot_product vdot (
             .op1(scalar_results[g]),
             .op2(scalar_results[g]),
@@ -51,16 +58,16 @@ module vector_normalize
         );
     end
 
-    for (g = 0; g < `FIXED_W; g = g + 1) begin : gen_leq
+    for (g = 0; g < `PIPELINE_STAGES; g = g + 1) begin : gen_leq
         assign packed_dot_results[g] = dot_results[g];
         assign dot_leq_one[g] = packed_dot_results[g] <= one;
-        assign test_bits[g] = (1 << (`FIXED_W - 1 - g));
+        assign test_bits[g] = (1 << (`PIPELINE_STAGES - 1 - g));
     end
 
     always @(posedge clk) begin
         scalars[0] = '0;
-        temp_ops[`FIXED_W:0] <= {temp_ops[`FIXED_W-1:0], op};
-        for (i = 0; i < `FIXED_W; i = i + 1) begin
+        temp_ops[`PIPELINE_STAGES:0] <= {temp_ops[`PIPELINE_STAGES-1:0], op};
+        for (i = 0; i < `PIPELINE_STAGES; i = i + 1) begin
             if ((~scalar_overflow[i] & ~dot_overflow[i]) & dot_leq_one[i]) begin
                 scalars[i + 1] <= scalars[i] | test_bits[i];
             end else begin
--- vector_math_test/test_vector_normalize.py
+++ vector_math_test/test_vector_normalize.py
@@ -1,26 +1,84 @@
 from . import *
-
+import logging
 import cocotb
-from cocotb.triggers import RisingEdge, ReadOnly
+from cocotb.triggers import RisingEdge, ReadOnly, Timer
 from cocotb.clock import Clock
+from cocotb.result import ReturnValue, TestFailure
 from testlib import *
 
-@cocotb.test()
-def normalize_test(dut):
-    cocotb.fork(Clock(dut.clk, 1, units="ns").start())
+class NormalizeTestbench:
+    def __init__(self, dut):
+        self.dut = dut
+        self.op = VectorSignal(dut.op)
 
-    op = VectorSignal(dut.op)
-    op.assign(make_fvec(4.1, 3.1, 15))
+        self.log = logging.getLogger(self.__class__.__name__)
+        self.log.setLevel(logging.INFO)
 
-    yield ReadOnly()
+        self.pipeline_latency = fixed_w
 
-    for _ in range(fixed_w):
-        yield RisingEdge(dut.clk)
+    @cocotb.coroutine
+    def initialize(self):
+        cocotb.fork(Clock(self.dut.clk, 1, units="ns").start())
+        yield Timer(0)
+
+    def set_inputs(self, op):
+        self.op.assign(op)
+
+    @cocotb.coroutine
+    def clock_input(self, op):
+        if self.pipeline_latency <= 0:
+            raise ValueError("Can't clock in any more inputs with this pipeline depth")
+
+        self.set_inputs(op)
+        yield RisingEdge(self.dut.clk)
+        self.pipeline_latency -= 1
+
+    @cocotb.coroutine
+    def get_result(self):
         yield ReadOnly()
-        print(dut.result)
 
-    v = dut.result.value
-    x, y, z = unpack_vector(v)
-    x, y, z = (float_from_fixed(i) for i in (x, y, z))
-    print(x, y, z)
-    print(x ** 2 + y ** 2 + z ** 2)
+        result = self.dut.result.value.integer
+        raise ReturnValue(unpack_vector(result))
+
+    @cocotb.coroutine
+    def assert_result(self, result):
+        result_actual = yield self.get_result()
+        
+        self.log.info("Operand: {}".format(self.dut.op.value))
+        self.log.info("Result:  {}".format(self.dut.result.value))
+
+        if (result != result_actual):
+            raise TestFailure("Result incorrect: Got {} Expected {}".format(result_actual, result))
+
+    @cocotb.coroutine
+    def clock_assert_result(self, result):
+        if self.pipeline_latency > fixed_w:
+            raise ValueError("This output is undefined for this pipeline depth")
+
+        yield self.assert_result(result)
+        yield RisingEdge(self.dut.clk)
+        self.pipeline_latency += 1
+
+    @cocotb.coroutine
+    def flush_pipeline(self):
+        for _ in range(self.pipeline_latency):
+            yield RisingEdge(self.dut.clk)
+
+@cocotb.test()
+def pipeline_test(dut):
+    tb = NormalizeTestbench(dut)
+    yield tb.initialize()
+
+    yield tb.clock_input(make_fvec(-4.1, 3.1, -15))
+    yield tb.clock_input(make_fvec(1, 2, 3))
+
+    yield tb.flush_pipeline()
+
+    yield tb.clock_assert_result(make_fvec(-0.25830078125, 0.1951904296875, -0.94482421875))
+    yield tb.clock_assert_result(unpack_vector(40379922796153575315879L))
+
+    yield tb.clock_input(make_fvec(3, 2, 1))
+
+    yield tb.flush_pipeline()
+
+    yield tb.clock_assert_result(make_fvec(0.8016357421875, 0.534423828125, 0.2672119140625))
