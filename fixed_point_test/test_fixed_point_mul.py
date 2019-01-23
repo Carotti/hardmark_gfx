fcd8a02354cb38190d08cb1e8c9c2cf2aed9dbd4
Thomas Carotti <thomas@carotti.co.uk>
2019-01-23 16:22:22 -0500

    More mul testcases

--- fixed_point_test/test_fixed_point_mul.py
+++ fixed_point_test/test_fixed_point_mul.py
@@ -46,16 +46,34 @@ def two_by_two_negative(dut):
     yield tb.assert_result(unpack_if(fixed_from_float(4)), 0)
 
 @cocotb.test()
+def two_by_two(dut):
+    tb = FixedPointTestbench(dut)
+    tb.set_inputs(unpack_if(fixed_from_float(2)), unpack_if(fixed_from_float(2)))
+    yield tb.assert_result(unpack_if(fixed_from_float(4)), 0)
+
+@cocotb.test()
 def ten_by_ten_negative(dut):
     tb = FixedPointTestbench(dut)
     tb.set_inputs(unpack_if(fixed_from_float(-10)), unpack_if(fixed_from_float(-10)))
     yield tb.assert_result(unpack_if(fixed_from_float(100)), 0)
 
 @cocotb.test()
-def found_vector_test(dut):
+def ten_by_ten(dut):
+    tb = FixedPointTestbench(dut)
+    tb.set_inputs(unpack_if(fixed_from_float(10)), unpack_if(fixed_from_float(10)))
+    yield tb.assert_result(unpack_if(fixed_from_float(100)), 0)
+
+@cocotb.test()
+def found_vector_test1(dut):
     tb = FixedPointTestbench(dut)
     tb.set_inputs(unpack_if(3760), unpack_if(16760832))
     yield tb.assert_result(unpack_if(7692960), 0)
+
+@cocotb.test()
+def found_vector_test2(dut):
+    tb = FixedPointTestbench(dut)
+    tb.set_inputs(unpack_if(fixed_from_float(0.05)), unpack_if(fixed_from_float(19.65576171875)))
+    yield tb.assert_result(unpack_if(8039), 0)
 
 num_equivalence_tests = 100
 
